c0b1a1cb3531959cddbab87ac314e5c355ef3f9e
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2024-06-14 11:44:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,107 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-import requests
+import asyncio
 
-url = "http://212.86.115.132:5000/api/video-watched"
+
+import sqlite3
+
+
+class Database:
+    def __init__(self, path_to_db="data/main.db"):
+        self.path_to_db = path_to_db
+
+    @property
+    def connection(self):
+        return sqlite3.connect(self.path_to_db)
+
+    def execute(self, sql: str, parameters: tuple = None, fetchone=False, fetchall=False, commit=False):
+        if not parameters:
+            parameters = ()
+        connection = self.connection
+        connection.set_trace_callback(logger)
+        cursor = connection.cursor()
+        data = None
+        cursor.execute(sql, parameters)
+
+        if commit:
+            connection.commit()
+        if fetchall:
+            data = cursor.fetchall()
+        if fetchone:
+            data = cursor.fetchone()
+        connection.close()
+        return data
+
+    def create_table_users(self):
+        sql = """
+        CREATE TABLE Users (
+            id int NOT NULL,
+            full_name varchar(100),
+            quiz_start varchar(100),
+            quiz_end varchar(100),
+            quiz_result int(3),
+            PRIMARY KEY (id)
+            );
+        """
+        self.execute(sql, commit=True)
+        
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        sql += " AND ".join([
+            f"{item} = ?" for item in parameters
+        ])
+        return sql, tuple(parameters.values())
+
+    def add_user(self, id: int, full_name: str,  quiz_start: str = "", quiz_end: str = "", quiz_result: str = ""):
+        sql = """
+        INSERT INTO Users(id, full_name, quiz_start, quiz_end, quiz_result) VALUES(?, ?, ?, ?, ?)
+        """
+        self.execute(sql, parameters=(id, full_name, quiz_start, quiz_end, quiz_result), commit=True)
+        
+        
+db = Database()
+try:
+    db.create_table_users()
+except:
+    pass
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/api/video-watched', methods=['POST'])
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+@app.route('/api/user_data', methods=['POST'])
 def video_watched():
     watch_data = request.get_json()
-    print(watch_data)
-    response = requests.post(url, json=watch_data)
-    print("bordi")
-    return jsonify({"message": "Data scanning..."})
+    user_id = int(watch_data.get('user_id'))
+    full_name = int(watch_data.get('full_name'))
+    quiz_start = int(watch_data.get('quiz_start'))
+    quiz_end = int(watch_data.get('quiz_end'))
+    quiz_result = int(watch_data.get('quiz_result'))
+    
+    db.add_user(id=int(user_id), full_name=full_name, quiz_start=quiz_start,
+                quiz_end=quiz_end, quiz_result=quiz_result)
+    
 
-@app.route('/', methods=['GET'])
+    print(watch_data)
+    return jsonify({"message": "Data added."})
+
+@app.route('/api', methods=['GET'])
 def video_watcheds():
     return jsonify({"message": "hello"})
 
+
+
+def logger(statement):
+    print(f"""
+_____________________________________________________        
+Executing: 
+{statement}
+_____________________________________________________
+""")
+    
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
